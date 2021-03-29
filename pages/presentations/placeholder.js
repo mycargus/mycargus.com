d02f238e25a4223d72d7dce357620c9db7ffcbac
Michael Hargiss <mdhargiss@gmail.com>
2021-03-28 23:52:04 -0700

    add presentations placeholder page

---
+++ pages/presentations/placeholder.js
@@ -0,0 +1,13 @@
+import styles from "../../styles/Home.module.css";
+
+export default function Placeholder() {
+  return (
+    <div className={styles.container}>
+      <main className={styles.main}>
+        <h1 className={styles.title}>Presentations</h1>
+
+        <p className={styles.description}>Coming soon!</p>
+      </main>
+    </div>
+  );
+}
