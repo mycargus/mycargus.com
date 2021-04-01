498ae5cd97579deed60a320c20a877be2e0f9c6c
Michael Hargiss <mdhargiss@gmail.com>
2021-04-01 16:06:43 -0700

    use absolute imports

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 import Head from 'next/head'
 import Link from 'next/link'
 import Emoji from 'a11y-react-emoji'
-import styles from '../styles/Home.module.css'
+import styles from 'styles/Home.module.css'
 
 export default function Home() {
   return (
